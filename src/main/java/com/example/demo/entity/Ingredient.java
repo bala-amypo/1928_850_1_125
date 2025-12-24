3b19d8631ac2471aefad08d59133f95eb4d821bb
bala-amypo <bala@amypo.in>
2025-12-24 04:07:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/Ingredient.java
+++ src/main/java/com/example/demo/entity/Ingredient.java
@@ -1,42 +1,23 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Table(name = "ingredients", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "name")
-})
+@Getter
+@Setter
 public class Ingredient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
     private String name;
 
     private String unit;
 
-    @Column(nullable = false)
-    private BigDecimal costPerUnit;
+    private double costPerUnit;
 
-    private boolean active = true;
-
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-    @PrePersist
-    public void onCreate() {
-        createdAt = LocalDateTime.now();
-        updatedAt = createdAt;
-    }
-
-    @PreUpdate
-    public void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
-
-    
+    private boolean active;
 }
